0cc21bb29a1b1f2c23f91ac612d5cc1ab1e92251
Ubuntu <student@ML-RefVm-869737.0rsh3rfi2jkurm0h1ravml0gra.ix.internal.cloudapp.net>
2021-04-01 02:01:23 +0000

    commented file

--- portfolio1/searchlog.sh
+++ portfolio1/searchlog.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-while true; do
+while true; do  # while loop to prompt user choices if to search a word or to exit
     if [[ $usercount -eq 0 ]]; then
         read -p 'Enter [1] to Search or [2] to Exit: '
     else
-        read -p 'Enter [1] to Search Again or [2] to Exit: '
+        read -p 'Enter [1] to Search Again or [2] to Exit: '    # an option to searc again or to exit after searching a word
     fi
     if [[ $REPLY -eq 2 ]]; then
         break
     else
         clear
         
-        read -p 'Enter search pattern: ' pattern
+        read -p 'Enter search pattern: ' pattern    # promt user to enter a word
         defaultifs=$IFS
         IFS=$'\n'
-        if grep -q $pattern access_log.txt; then
-            while true
+        if grep -q $pattern access_log.txt; then    # if statement to verify if the word is in the text file
+            while true  # while loop that while true will loop through the menu bar
             do
                 clear
                 echo "Choose an item: 1,2,3 or 4"
@@ -25,7 +25,7 @@ while true; do
                 echo "4; Exit"
                 read -sn1
                 clear
-                case "$REPLY" in
+                case "$REPLY" in    # case statement that will execute depending with the user's choice in the menu
                     1) echo "$(grep -wic "$pattern" access_log.txt) matches found" 
                        grep -win "$pattern" access_log.txt;;
                     2) echo "$(grep -cin "$pattern" access_log.txt) matches found"
@@ -34,14 +34,14 @@ while true; do
                        grep -vin "$pattern" access_log.txt;;
                     4) break;;
                 esac
-                read -n1 -p "Press any key to continue"
+                read -n1 -p "Press any key to continue" # added feature to prompt the user to go back to the menu if he wants a different option
             done
         else
             echo "No matches found"     
         fi      
     fi
-    ((usercount++))
-    IFS=$defaultifs
+    ((usercount++)) # add one usercount
+    IFS=$defaultifs # set IFS back to default
     echo ""
 done
-exit 0
+exit 0  # exit the script
